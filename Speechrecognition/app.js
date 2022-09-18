8eb18735224e240ef90cb302ba8b2a2126f2ba63
Kit Calag <39394330+calagkit100@users.noreply.github.com>
2022-09-18 13:04:47 +0800

    Add files via upload

---
+++ Speechrecognition/app.js
@@ -0,0 +1,57 @@
+const startBtn = document.getElementById("start");
+const stopBtn = document.getElementById("stop");
+
+
+// speech   recognition setup
+
+const speechRecognition =
+    window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new speechRecognition();
+
+//sr start
+// recognition.onstart = function() {
+//     console.log("mic activated");
+
+// };
+
+//sr stop
+// recognition.onend = function() {
+//     console.log("mic deactivated");
+// };
+
+//sr result
+
+recognition.onresult = function(event) {
+    let current = event.resultIndex;
+    let transcript = event.results[current][0].transcript;
+    console.log(transcript);
+}
+
+//sr continous
+// recognition.continuous = true;
+
+//event listener
+startBtn.addEventListener("click", () => {
+    recognition.start()
+});
+
+stopBtn.addEventListener("click", () => {
+    recognition.stop()
+});
+
+
+sendbutton.onclick = async() => {
+
+    const value = transcript
+    const res = await fetch("http://localhost:3000/complete", {
+        body: JSON.stringify({
+            query: value
+        }),
+        method: 'POST',
+        headers: {
+            'Content-Type': 'Application/json'
+        }
+    })
+    const data = await res.json()
+};
